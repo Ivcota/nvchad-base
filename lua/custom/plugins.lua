273f0ae585d1ad10a14009e6a62f3f9c440f003e
Iverson Diles <ivcotad@gmail.com>
2024-02-10 12:16:35 -0700

    working copiolt

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -7,9 +7,6 @@ local plugins = {
     end
   },
   {
-    "github/copilot.vim",
-  },
-  {
     'williamboman/mason.nvim',
     opts = {
       ensure_installed = {
@@ -56,6 +53,36 @@ local plugins = {
   },
   "Slotos/telescope-lsp-handlers.nvim",
   "nvim-lua/plenary.nvim",
+  -- ... all your other plugins ...
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = function()
+      require('copilot').setup()
+    end
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+          require("copilot_cmp").setup()
+        end,
+      },
+    },
+    opts = {
+      sources = {
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "copilot",  group_index = 2 },
+        { name = "luasnip",  group_index = 2 },
+        { name = "buffer",   group_index = 2 },
+        { name = "nvim_lua", group_index = 2 },
+        { name = "path",     group_index = 2 },
+      },
+    },
+  },
 }
 
 return plugins
