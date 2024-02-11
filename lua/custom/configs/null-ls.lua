f6a1c0f09b3ed03c814943629f5d5052a31528cd
Iverson Diles <ivcotad@gmail.com>
2024-02-10 17:15:12 -0700

    add formatter to null_ls

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -2,7 +2,8 @@ local null_ls = require('null-ls')
 
 local opts = {
   sources = {
-    null_ls.builtins.formatting.prettierd
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.black
   }
 }
 
